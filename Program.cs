17b43c7268e9f9b3b3e3378bacc0c0ebae54c700
isabel <isabel.calzadilla.18@gmail.com>
2022-04-27 16:53:32 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories); // MÈTODO QUE UBICA ARCHIVOS EN UNA CARPETA
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+Console.WriteLine($@"Este es el directorio  =     
+                            {Directory.GetCurrentDirectory()}"); //MÈTODO QUE UBICA EL DIRECOTIRO ACTUAL
